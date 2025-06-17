66cf31569f00f9e342e32d2a794b46450f642ee6
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-06-17 13:56:26 -0300

    update usecase, adapter, controller

--- src/main/java/com/example/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/example/hexagonal/adapters/in/controller/CustomerController.java
@@ -2,14 +2,13 @@ package com.example.hexagonal.adapters.in.controller;
 
 import com.example.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.example.hexagonal.adapters.in.controller.request.CustomerRequest;
+import com.example.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.example.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.example.hexagonal.application.ports.in.InsertCustomerInputPort;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -17,6 +16,9 @@ public class CustomerController {
 
     @Autowired
     private InsertCustomerInputPort insertCustomerInputPort;
+
+    @Autowired
+    private FindCustomerByIdInputPort findCustomerByIdInputPort;
 
     @Autowired
     private CustomerMapper customerMapper;
@@ -29,4 +31,14 @@ public class CustomerController {
 
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
+
+        var customer = findCustomerByIdInputPort.find(id);
+        var customerResponse = customerMapper.toCustomerResponse(customer);
+
+        return ResponseEntity.ok().body(customerResponse);
+    }
+
 }
