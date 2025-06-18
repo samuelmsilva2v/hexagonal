9ef1d115f3fc7714ff5bcdb5f01dd11ce52b166f
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-06-18 09:43:57 -0300

    kafka consumer, producer

--- src/main/java/com/example/hexagonal/application/core/usecase/InsertCustomerUseCase.java
+++ src/main/java/com/example/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -4,6 +4,7 @@ import com.example.hexagonal.application.core.domain.Customer;
 import com.example.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.example.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
 import com.example.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import com.example.hexagonal.application.ports.out.SendCpfForValidationOutputPort;
 
 public class InsertCustomerUseCase implements InsertCustomerInputPort {
 
@@ -11,9 +12,12 @@ public class InsertCustomerUseCase implements InsertCustomerInputPort {
 
     private final InsertCustomerOutputPort insertCustomerOutputPort;
 
+    private final SendCpfForValidationOutputPort sendCpfForValidationOutputPort;
+
     public InsertCustomerUseCase(
             FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
-            InsertCustomerOutputPort insertCustomerOutputPort
+            InsertCustomerOutputPort insertCustomerOutputPort,
+            SendCpfForValidationOutputPort sendCpfForValidationOutputPort
     ) {
         this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
         this.insertCustomerOutputPort = insertCustomerOutputPort;
@@ -26,5 +30,7 @@ public class InsertCustomerUseCase implements InsertCustomerInputPort {
         customer.setAddress(address);
 
         insertCustomerOutputPort.insert(customer);
+
+        sendCpfForValidationOutputPort.send(customer.getCpf());
     }
 }
---
+++ src/main/java/com/example/hexagonal/application/ports/out/SendCpfForValidationOutputPort.java
@@ -0,0 +1,6 @@
+package com.example.hexagonal.application.ports.out;
+
+public interface SendCpfForValidationOutputPort {
+
+    void send(String cpf);
+}
